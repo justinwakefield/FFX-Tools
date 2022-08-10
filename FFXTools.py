9a64c7084105915ac11f11d8815c47abe72765d3
justinwakefield <106996183+justinwakefield@users.noreply.github.com>
2022-08-10 15:16:58 +0200

    Add files via upload

---
+++ FFXTools.py
@@ -0,0 +1,102 @@
+import keyboard
+import time
+import mss
+from PIL import Image, ImageStat
+
+KEYPRESS = 0.1
+CHANGEKEY = 0.4
+SCREEN_LOCATION = {"top": 400, "left": 40, "width": 40, "height": 40}
+COLOR_MINIMUM = 150
+sct = mss.mss()
+
+
+def keypress(key):
+    time.sleep(CHANGEKEY)
+    keyboard.press(key)
+    time.sleep(KEYPRESS)
+    keyboard.release(key)
+
+
+def check_if_white():
+    sct_img = sct.grab(SCREEN_LOCATION)
+    img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
+    extrema = ImageStat.Stat(img).extrema
+    return all(e[0] > COLOR_MINIMUM for e in extrema)
+
+
+def lightning_dodge():
+    last_is_white = False
+    dodge_count = 0
+    while True:
+        is_white = check_if_white()
+        if is_white and not last_is_white:
+            keyboard.press('c')
+            time.sleep(KEYPRESS)
+            keyboard.release('c')
+            dodge_count += 1
+            print(f"{dodge_count}")
+        last_is_white = is_white
+
+
+print("============================")
+print("FINAL FANTASTY X HD Tools v2")
+print("============================")
+print("Movement keys must be bound to WASD")
+print("Confirm, Menu, and Cancel mapped to C, V, X respectively\n")
+
+print("Press '1' for Autobattler")
+print("Press '2' for Yuna Party Heal")
+print("Press '3' for Lightning Dodger\n")
+print("Press 'P' to Pause.")
+print("Press 'Q' to Quit Program.")
+
+
+while not keyboard.is_pressed('q'):
+    if keyboard.is_pressed('q'):
+        quit()
+# AUTOBATTLER
+    if keyboard.is_pressed('1'):
+        keypress("f1")
+        keypress("f1")
+        keypress("f2")
+        keypress("f3")
+        keypress("f4")
+        while not keyboard.is_pressed('p'):
+            if keyboard.is_pressed('q'):
+                keypress("f1")
+                keypress("f2")
+                keypress("f3")
+                keypress("f3")
+                keypress("f4")
+                quit()
+            keyboard.press('c')
+            keyboard.press('d')
+            time.sleep(0.4)
+            keyboard.release('d')
+            keyboard.press('a')
+            time.sleep(0.4)
+            keyboard.release('a')
+# YUNA PARTY HEAL
+    if keyboard.is_pressed('2'):
+        keypress("v")
+        keypress("s")
+        keypress("s")
+        keypress("c")
+        keypress("w")
+        keypress("c")
+        keypress("c")
+        for heal in range(3):
+            for cure in range(5):
+                keypress("c")
+            keypress("s")
+        for back in range(3):
+            keypress("x")
+    if keyboard.is_pressed('3'):
+        keypress("f1")
+        keypress("f1")
+        keypress("f3")
+        keypress("f3")
+        while not keyboard.is_pressed('p'):
+            lightning_dodge()
+            if keyboard.is_pressed('q'):
+                quit()
---
+++ README.txt
@@ -0,0 +1,23 @@
+FFX Tools
+
+GENERAL SETUP
+Movement keys must be bound to WASD
+Confirm, Menu, and Cancel mapped to C, V, X respectively
+
+You can use the 2nd Keyboard settings to set these if they're not your default.
+
+
+
+Autofarmer
+SETUP: MOVE TO AREA YOU WANT TO FARM
+Very simple script that moves Tidus around and holds Confirm in order to AFK farm.
+
+
+Yuna Party Heals
+SETUP: YUNA MUST BE IN 8TH PARTY SLOT
+Some people are less inclined to want to "cheat" but just automatically gaining HP indefinitely through the use of the supercharger boost.
+For those people, the Yuna Party Heals tool allows you to go from the regular game display in order to heal all 3 current party members.
+
+
+Lightning Dodger
+Forked from: https://gist.github.com/fritz-c/f341a1b588564c0a73ffddd684151acf
